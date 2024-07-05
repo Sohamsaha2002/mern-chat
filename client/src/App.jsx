84a977c2a3025a3d3c41650b2ed67c4ecdf40157
SOHAM SAHA <97799613+Sohamsaha2002@users.noreply.github.com>
2024-07-05 23:11:50 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Routes from "./Routes";
 import axios from "axios";
 import { UserContextProvider } from "./UserContext";
 function App() {
-  axios.defaults.baseURL = "http://localhost:4040";
+  axios.defaults.baseURL = "https://schat-m29e.onrender.com";
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
